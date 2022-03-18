6a779934b77389b9d15ad0e3bd2c3d4d25f13d8e
rachid <bobirachid@gmail.com>
2022-03-18 23:02:56 +0100

    change to php

---
+++ quote of the day/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="Quote">
+	<title>Quote of The Day</title>
+	<link rel="stylesheet" href="main.css"> 
+</head>
+
+<body>
+	<h2>quote of the day</h2>
+	<h4>Press the Button Below to receive a random quote</h4>
+	<button onclick="generateQuote()">New quote</button>
+
+	<p id="quote1"></p>
+	<br>
+	<p id="author"></p>
+	<p id= "car"></p>
+	<br><br>
+	<button id="enterAuto" onclick="carBecome()" >Change Car</button>
+
+<script src="main.js"></script>
+</body>
+</html>
